a8a62b6e50f3554946dbf926124f3971d76455b5
AntonyBil <beletskiy.ant@gmail.com>
2023-08-30 22:34:29 +0300

    Initial commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by apple on 30.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
